bf9fd323d08f7d39fc3da4300d870dc7148385e0
Muh. Gazali Saleh <44740772+gazalisaleh@users.noreply.github.com>
2020-10-03 12:40:01 +0700

    Updates

---
+++ precache-manifest.3435adecf29145329a6b24a3f6fa4e10.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2682706b2f5500b8e1013d7f890ec5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5d999f2a972724a2aa2",
+    "url": "/static/css/main.623de801.chunk.css"
+  },
+  {
+    "revision": "3b833d29c32496cfbce3",
+    "url": "/static/js/2.8cf464eb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8cf464eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5d999f2a972724a2aa2",
+    "url": "/static/js/main.bcbd63da.chunk.js"
+  },
+  {
+    "revision": "8ce0042f73ea39751ea5",
+    "url": "/static/js/runtime-main.c8bcae13.js"
+  },
+  {
+    "revision": "7802d8b27fcb19893ce6b38c0789268e",
+    "url": "/static/media/Raleway-Bold.7802d8b2.ttf"
+  },
+  {
+    "revision": "6c084270ccdeb72fd9f5a5144cea628f",
+    "url": "/static/media/Raleway-Light.6c084270.ttf"
+  },
+  {
+    "revision": "61d74ada6f9397fe02b7576570f96321",
+    "url": "/static/media/mainheader.61d74ada.png"
+  },
+  {
+    "revision": "a22effeba741dc738973b95e38466980",
+    "url": "/static/media/me.a22effeb.png"
+  },
+  {
+    "revision": "af18ec055ee47d30b2e3c85cc6fa1a4f",
+    "url": "/static/media/networkicon.af18ec05.png"
+  }
+]);
