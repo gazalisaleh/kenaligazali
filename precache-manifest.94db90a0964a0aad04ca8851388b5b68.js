4f4520fbae38ae1f121abd0e624a145afc1eda65
Muh. Gazali Saleh <44740772+gazalisaleh@users.noreply.github.com>
2020-10-03 12:45:54 +0700

    Updates

---
+++ precache-manifest.94db90a0964a0aad04ca8851388b5b68.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be756c7ecc0fea821c2e59e5f8063c7",
+    "url": "/kenaligazali/index.html"
+  },
+  {
+    "revision": "670fea1d2b951dd32027",
+    "url": "/kenaligazali/static/css/main.c3bb6a6e.chunk.css"
+  },
+  {
+    "revision": "3b833d29c32496cfbce3",
+    "url": "/kenaligazali/static/js/2.8cf464eb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/kenaligazali/static/js/2.8cf464eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "670fea1d2b951dd32027",
+    "url": "/kenaligazali/static/js/main.68d5fd22.chunk.js"
+  },
+  {
+    "revision": "3ecb10853001ffd58363",
+    "url": "/kenaligazali/static/js/runtime-main.b334fb6e.js"
+  },
+  {
+    "revision": "7802d8b27fcb19893ce6b38c0789268e",
+    "url": "/kenaligazali/static/media/Raleway-Bold.7802d8b2.ttf"
+  },
+  {
+    "revision": "6c084270ccdeb72fd9f5a5144cea628f",
+    "url": "/kenaligazali/static/media/Raleway-Light.6c084270.ttf"
+  },
+  {
+    "revision": "61d74ada6f9397fe02b7576570f96321",
+    "url": "/kenaligazali/static/media/mainheader.61d74ada.png"
+  },
+  {
+    "revision": "a22effeba741dc738973b95e38466980",
+    "url": "/kenaligazali/static/media/me.a22effeb.png"
+  },
+  {
+    "revision": "af18ec055ee47d30b2e3c85cc6fa1a4f",
+    "url": "/kenaligazali/static/media/networkicon.af18ec05.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3435adecf29145329a6b24a3f6fa4e10.js"
+  "/kenaligazali/precache-manifest.94db90a0964a0aad04ca8851388b5b68.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kenaligazali/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
